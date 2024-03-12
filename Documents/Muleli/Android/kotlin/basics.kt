199326215c66c2b9e1df0e739866141d7c36f861
kelvin <mulelikelvin1@gmail.com>
2024-03-12 08:07:54 +0300

    wwwww

---
+++ Documents/Muleli/Android/kotlin/basics.kt
@@ -0,0 +1,12 @@
+fun main(){
+    println("my first kotlin program")
+
+    println("New line")
+
+morning()
+
+}
+fun morning(){
+
+    println("The morning function")
+}
---
+++ Documents/Muleli/Android/kotlin/variables.kt
@@ -0,0 +1,16 @@
+fun main(){
+    var name: String = "Lordivicus"
+    name = "Edwin"
+
+    val english: Double = 80.7
+    //val big:char = 'f'
+    val small:Boolean = true
+    val medium:Int = 7
+
+
+    println(name)
+    println(english)
+//    println(big)
+    println(small)
+    println(medium)
+}
